5dcaa5408ad82922623d7be5928b1f4bf8d0338a
Sabin Lehaci <57048754+saboel@users.noreply.github.com>
2022-12-11 23:30:58 -0600

    first

---
+++ src/application.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'; 
+
+class Application extends React.Component {
+    render () {
+        return (
+            <div>
+            <span>
+                Hello, this site is under construction please
+                check back later. 
+            </span>
+            <p>
+                While you're here, this site will be a collection 
+                of blog posts. The information of posts will not only be limited
+                to esoteric technology but a collage of my ideas brought to life. 
+                
+            </p>
+            </div>
+        );
+    }
+}
+
+export default Application; 
+
+
